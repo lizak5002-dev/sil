73f1e353db8db30c46983aac5e1dc33b6147fead
lizak5002-dev <lizak5002@gmail.com>
2025-12-15 22:14:55 +0500

    add more failes

--- users/views.py
+++ users/views.py
@@ -1,4 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm
 
-# Create your views here.
 #hello Tasya moy lubimyu
+
+def index(request):
+    return render(request, 'index.html', {'title': 'Главная страница'})
+    
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            return redirect()
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form':form, 'title':'Регистрация'})
+
