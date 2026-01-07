aed68019e1f67acc10a3ca30b7c0c8c7257dc176
Elizaveta <lizak5002@gmail.com>
2026-01-07 21:18:38 +0500

    myay

--- core/settings.py
+++ core/settings.py
@@ -117,6 +117,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
---
+++ notes/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Note
+
+class NoteForm(forms.ModelForm):
+    class Meta:
+        model = Note
+        fields = ['title', 'context']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите название'}),
+            'context': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите текст'})
+        }
+        labels = {
+            'title': 'Название заметки',
+            'context': 'Текст заметки'
+        }
